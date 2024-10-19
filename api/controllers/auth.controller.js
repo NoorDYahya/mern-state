35647fd0d36ed6d74a3546ba4d645bc7ac5c55ec
noordaryahya <noor.daryahya@mail.huji.ac.il>
2024-10-19 18:25:21 +0300

    create middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup = async (req,res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req,res,next) => {
 
     const {username,email,password} = req.body;
 
@@ -13,7 +14,7 @@ export const signup = async (req,res) => {
         res.status(201).json("user cretaed successfully!");
     }
     catch(error){
-        res.status(500).json(error);
+        next(error);
     }
 
 
