eef8287e7eb894c6c550e10a940e322fbc3f46b9
noordaryahya <noor.daryahya@mail.huji.ac.il>
2024-10-27 19:09:36 +0200

    add delete user functionality

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -34,3 +34,17 @@ export const updateUser  = async(req,res,next) => {
         next(error);
     }
 };
+
+export const deleteUser = async (req,res,next)=> {
+    if(req.user.id !== req.params.id) next(errorHandler(401, 'You only own your own account!'));
+
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie('access_token'); // must be delted before creating the response
+        res.status(200).json('User has been deleted!');
+
+    } catch (error) {
+        next(error);
+    }
+
+};
