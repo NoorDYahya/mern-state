083da2208ade66f97689a98cc46f2bd1e593e4c8
noordaryahya <noor.daryahya@mail.huji.ac.il>
2024-10-29 14:09:28 +0200

    create get user listings api route

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req,res) =>{
     res.json({
@@ -43,6 +44,18 @@ export const deleteUser = async (req,res,next)=> {
         res.clearCookie('access_token'); // must be delted before creating the response
         res.status(200).json('User has been deleted!');
 
+    } catch (error) {
+        next(error);
+    }
+
+};
+
+export const getUserListing = async (req,res, next) =>{
+
+    if(req.user.id !== req.params.id ) next(errorHandler(401,'You only can view your own listings!'));
+    try {
+        const listings = await Listing.find({userRef: req.params.id});
+        res.status(200).json(listings);
     } catch (error) {
         next(error);
     }
